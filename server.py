8edefc5899e40eabd1ec0d720fa0eec58ea84681
batatavadaX <75261987+batatavadaX@users.noreply.github.com>
2021-03-27 08:48:57 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,1 +1,24 @@
+# importing folder.
+import sys
+sys.path.insert(
+    1, 
+    'web'
+)
 
+# importing env & main app.
+import os
+
+# import dotenv
+from web import horse
+HOST=os.environ.get("HOST", "0.0.0.0")
+PORT=int(os.environ.get("PORT", 6969))
+
+#run a standard server.
+if __name__ == "__main__":
+	
+	horse.run(
+	    host=HOST,
+	    port=PORT,
+	    debug=True
+	)
+© 2
